ea2f6106919542b7defe7e937989e12b78265278
Dencer <tdaddon@163.com>
2021-07-02 10:06:33 +0800

    fix: error

--- LibShowUIPanel-1.0.lua
+++ LibShowUIPanel-1.0.lua
@@ -3,7 +3,7 @@
 -- @Link   : https://dengsir.github.io
 -- @Date   : 6/15/2021, 11:20:01 PM
 --
-local MAJOR, MINOR = 'LibShowUIPanel-1.0', 3
+local MAJOR, MINOR = 'LibShowUIPanel-1.0', 4
 
 ---@class LibShowUIPanel-1.0
 local Lib, oldminor = LibStub:NewLibrary(MAJOR, MINOR)
@@ -106,7 +106,7 @@ function Lib.Toggle(frame)
     end
 end
 
-if oldminor < 3 then
+if not oldminor or oldminor < 3 then
     -- 可长期持有的API
     function Lib.ShowUIPanel(frame, force)
         return Lib.Show(frame, force)
